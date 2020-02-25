1f3b47676f24c76ff9cbe572eb892d60cc13daf5
Arthur Nangai <arthur.nangai@gmail.com>
2020-02-25 23:14:39 +0300

    feat: Create new actor. (#2)
    - Add endpoint to create new actor.
    - Modify base model.

--- app/views/actor.py
+++ app/views/actor.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify, request
-from app.models.actor import Actor
+from flask import Blueprint, jsonify, request, abort
+from sqlalchemy.exc import DatabaseError, DataError
 
+from app.models.actor import Actor
 
 bp = Blueprint('actors', __name__, url_prefix='/actors')
 
@@ -14,3 +15,33 @@ def get_actors():
         "success": True,
         "actors": actor_list
     })
+
+
+@bp.route('', methods=['POST'])
+def create_actor():
+    """Adds a new actor"""
+    body = request.get_json()
+    validated_body = {}
+    required_fields = ['name', 'age', 'gender']
+    for field in required_fields:
+        resp = body.get(field, None)
+        if resp is None:
+            abort(400)
+        validated_body[field] = resp
+
+    try:
+        new_actor = Actor(
+            name=validated_body['name'],
+            age=validated_body['age'],
+            gender=validated_body['gender'])
+        new_actor.insert()
+        actors = Actor.query.all()
+        actor_list = [actor.format() for actor in actors]
+        return jsonify({
+            'success': True,
+            'created': new_actor.id,
+            'actors': actor_list,
+            'total_actors': len(actor_list)
+        }), 201
+    except DatabaseError:
+        abort(422)
