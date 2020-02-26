497674e92ebeacfb22bef662ff6c25d7a24fca29
arthurarty <arthur.nangai@gmail.com>
2020-02-26 20:11:18 +0300

    chore: Add movie tests.

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,6 +1,9 @@
+import json
+
+from app.models.movie import Movie
 from tests.base import BaseTest
 from tests.sample_data.movie import bad_request, movie1, unproccessable_request
-from utils.test_utils import post_json, post_json_header
+from utils.test_utils import post_json_header
 
 
 class MovieTestCase(BaseTest):
@@ -9,25 +12,52 @@ class MovieTestCase(BaseTest):
     def setUp(self):
         super().setUp()
 
+    def create_movie(self):
+        """Create a movie to test patch"""
+        new_movie = Movie(**movie1)
+        new_movie.insert()
+        return new_movie
+
     def test_get_movie(self):
         """Test API returns movie list"""
         res = self.client().get('/movies')
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
     def test_create_movie(self):
         """Ensure API can create movie"""
-        res = post_json(
-            self.client, '/movies', movie1)
+        res = self.client().post('/movies', json=movie1)
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
 
     def test_bad_request(self):
         """Ensure API returns 400 status code"""
-        res = post_json(
-            self.client, '/movies', bad_request)
+        res = self.client().post('/movies', json=bad_request)
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
 
     def test_unprocessable_request(self):
         """Ensure API returns 422 error"""
-        res = post_json(
-            self.client, '/movies', unproccessable_request)
+        res = self.client().post('/movies', json=unproccessable_request)
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+
+    def test_patch_movie(self):
+        """Ensure API allows editing movies."""
+        movie = self.create_movie()
+        res = self.client().patch(f'/movies/{movie.id}', json=bad_request)
+        data = json.loads(res.data)
+        self.assertAlmostEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_patch_404(self):
+        """Ensure API returns 404 for movie
+        that does not exist"""
+        res = self.client().patch(f'/movies/1000', json=bad_request)
+        data = json.loads(res.data)
+        self.assertAlmostEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
--- utils/test_utils.py
+++ utils/test_utils.py
@@ -1,22 +1,9 @@
-
 import json
 
 
-def json_of_response(response):
-    """Decode json from response"""
-    return json.loads(response.data.decode('utf8'))
-
-
-def post_json(client, url, json_dict):
-    """Send dictionary json_dict as a json to the specified url """
-    return client().post(
-        url, data=json.dumps(json_dict), content_type='application/json')
-
-
 def post_json_header(client, url, json_dict, token):
-    """Send dictionary json_dict as a json to the specified url """
+    """send post request with header"""
     return client().post(
         url,
-        data=json.dumps(json_dict),
-        content_type='application/json',
+        json=json_dict,
         headers={'Authorization': 'Bearer ' + token})
